5b012866e40cf256ffdde0b12b674403fe0ebb10
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-06-24 10:32:01 +0300

    e3

---
+++ Lesson_6-Programirovanie/Example2/Program.cs
@@ -0,0 +1,5 @@
+﻿
+        Console.WriteLine("Your name please ");
+        string username = Console.ReadLine();
+        Console.WriteLine("Hello, ");
+        object value = Console.WriteLine(username);
