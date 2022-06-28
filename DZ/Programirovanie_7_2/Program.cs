0bc8564f30a43bd924c915f29ba07d98ff6af477
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-06-28 09:30:13 +0300

    Finished Seminar 7-1

--- DZ/Programirovanie_7_2/Program.cs
+++ DZ/Programirovanie_7_2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Write("Write number A:");
-int a = Convert.ToInt32(Console.ReadLine()); // число A
+int a = Convert.ToInt32(Console.ReadLine()); // число B
 Console.Write("Write number B:");
 int b = Convert.ToInt32(Console.ReadLine()); // число B
 Console.Write("Write number C:");
