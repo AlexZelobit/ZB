059f97396279c48f4ee29ceb05bce52b1325bc25
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-06-28 08:51:25 +0300

    dz-7-2

--- DZ/Programirovanie_7_2/Program.cs
+++ DZ/Programirovanie_7_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Write number A:");
+int a = Convert.ToInt32(Console.ReadLine()); // число A
+Console.Write("Write number B:");
+int b = Convert.ToInt32(Console.ReadLine()); // число B
+Console.Write("Write number C:");
+int c = Convert.ToInt32(Console.ReadLine()); // число C
+int max = a;
+if (b > max)
+{
+              max = b;
+}
+if (c > max)
+{
+              max = c;
+}
+Console.WriteLine("Max number is " + max);
