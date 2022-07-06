2b70060f79da3450ca3f22e2dc3dc29a03f67c24
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-07-06 11:30:59 +0300

    end

--- Seminar/S-7-2/Zada4a_7-4/Program.cs
+++ Seminar/S-7-2/Zada4a_7-4/Program.cs
@@ -1,33 +1,27 @@
-﻿// Программа принимает 2 числа и выводит ответ является ли первое кратным второму. Если оно не кратно то выводит остаток от деления.
-// 31, 5 -> не кратно, остаток 4
-// 16, 4 -> кратно
+﻿// Программа принимает число и проверяет кратно ли оно одновременно 7 и 23
+
 // Если условие ИЛИ то || если И то &&
 // решение автоматическое
-int randomNumber1 = new Random().Next(16, 30);
-int randomNumber2 = new Random().Next(2, 4);
-Console.WriteLine($"Первое число {randomNumber1}");
-Console.WriteLine($"Второе число {randomNumber2}");
-if (randomNumber1 % randomNumber2 == 0)
+int randomNumber1 = new Random().Next(7, 46);
+Console.WriteLine($"число {randomNumber1}");
+if (randomNumber1 % 7 == 0 && randomNumber1 % 23 == 0)
 {
              Console.WriteLine($"Кратно"); 
 }
 else
 {
-              Console.WriteLine($"Не кратно остаток {randomNumber1 % randomNumber2}");
+              Console.WriteLine($"Не кратно");
 }
 
 // решенеие с водом чисел
 Console.WriteLine(" Write First number is ...");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Write Second number is ...");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Первое число {a}");
-Console.WriteLine($"Второе число {b}");
-if (a % b == 0)
+Console.WriteLine($"число {a}");
+if (a % 7 == 0 && a % 23 == 0)
 {
              Console.WriteLine($"Кратно"); 
 }
 else
 {
-              Console.WriteLine($"Не кратно остаток {a % b}");
+              Console.WriteLine($"Не кратно");
 }
