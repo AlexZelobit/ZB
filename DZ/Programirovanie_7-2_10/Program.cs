797b78f0a72dd700213c89f78ea593fc9c14dd66
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-07-07 10:15:17 +0300

    finished 7-2

--- DZ/Programirovanie_7-2_10/Program.cs
+++ DZ/Programirovanie_7-2_10/Program.cs
@@ -1,9 +1,6 @@
-﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-Console.Write("Write number:");
-int a = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-while (result <= a)
-              {
-              Console.Write(result + " ");
-              result++;
-              }
+﻿// Напишите программу, которая на вход принимает трехзначноечисло, а на выходе показывает вторую цифру этого числа.
+int randomNumber = new Random().Next(99, 1000);
+int firstDigit = randomNumber / 100; // узнаем первую цифру
+int thirdDigit = randomNumber % 10; // узнаем третью через остаток
+int secondDigit = (randomNumber - firstDigit * 100 - thirdDigit) / 10;
+Console.WriteLine($"Число {randomNumber} вторая цифра {secondDigit}");
--- DZ/Programirovanie_7-2_15/Program.cs
+++ DZ/Programirovanie_7-2_15/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.Write("Write number:");
 int a = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-while (result <= a)
-              {
-              Console.Write(result + " ");
-              result++;
-              }
+if (a == 6 || a == 7)
+{
+        Console.WriteLine($"День {a} выходной");      
+}
+else
+{
+        Console.WriteLine($"День {a} не выходной");  
+}
