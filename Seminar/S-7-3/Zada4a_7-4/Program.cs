cebaf1b4173d39c9300301956d84202785638d19
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-07-08 14:19:30 +0300

    s-7-4

--- Seminar/S-7-3/Zada4a_7-4/Program.cs
+++ Seminar/S-7-3/Zada4a_7-4/Program.cs
@@ -1,43 +1,17 @@
-﻿// Программа принимает координаты двух точек и показывает расстояние между ними
-// применить теорему пифагора = гипотенуза = корень из суммы квадратов катетов
-/* А (5,3) и В(4,2) Длинна будет = V(5-4)^2+(3-2)^2
-while(true) // повторяем пока программа не выполнится
+﻿// Программа принимает число X и выдает квадраты числа от 1 до X
+// 5 -> 1 4 9 16
+// Методы
+int InputIn (string output) // метод для ввода числа
 {
-              Console.Clear(); // очищаем значения после цикла
-              int number = InputIn("Введите номер четверти: ");
-              int InputIn (string output) // метод для ввода числа
-                            {
-                                 Console.Write(output);
-                                 return Convert.ToInt32(Console.ReadLine());
-                            }
-              if (number > 4)
-                            {
-                                 Console.WriteLine("Четверть должны быть < 4");
-                                 return; // выходим из функции
-                            }
-           if (number > 0)         
-           {        
-              if (number == 1)
-                            {
-                                 Console.WriteLine("x и у должны быть > 0");
-                            }
-              else if (number == 2)
-                            {
-                                 Console.WriteLine("x < 0 && y > 0");
-                            }
-              else if (number == 3)
-                            {
-                                 Console.WriteLine("x < 0 && y < 0");
-                            }
-              else if (number == 4)
-                            {
-                                 Console.WriteLine("x > 0 && y < 0");
-                            }     
-           }
-           else
-                            {
-                                 Console.WriteLine("Четверть не должна быть отрицательной");
-                            }   
-
-              break; // прервать функцию
+     Console.Write(output);
+     return Convert.ToInt32(Console.ReadLine());
 }
+int Quadro (string number) // метод для получения квадрата числа
+{
+              int Quadro = number * number;
+              return Quadro;
+}
+//Решение
+     int number = InputIn("Введите число: ");
+     int numberQuadro = Quadro(number);
+                                 Console.WriteLine($"Число {number} его квадрат {numberQuadro}");
