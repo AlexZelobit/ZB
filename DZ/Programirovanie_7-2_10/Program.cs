d3db25cc23c6111ab778fde37ed5ef40571232a4
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-07-09 22:52:56 +0300

    DZ

--- DZ/Programirovanie_7-2_10/Program.cs
+++ DZ/Programirovanie_7-2_10/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая на вход принимает трехзначноечисло, а на выходе показывает вторую цифру этого числа.
-int randomNumber = new Random().Next(99, 1000);
+int randomNumber = new Random().Next(100, 999);
 int firstDigit = randomNumber / 100; // узнаем первую цифру
 int thirdDigit = randomNumber % 10; // узнаем третью через остаток
 int secondDigit = (randomNumber - firstDigit * 100 - thirdDigit) / 10;
