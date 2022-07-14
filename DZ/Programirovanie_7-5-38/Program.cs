de8a7492a05cc92abd5b96456fcb6337480cb2e1
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-07-15 00:34:27 +0300

    Finish 7-6

--- DZ/Programirovanie_7-5-38/Program.cs
+++ DZ/Programirovanie_7-5-38/Program.cs
@@ -1,8 +1,68 @@
 ﻿// Массив вещественных чисел. Найти разницу между минимальным и максимальным значением массива
 
+int size = GetSizeArray("Введите длинну массива: ");
+double[] userArray = GetArray(size);
+PrintArray(userArray);
+double maxNumber = userArray[0],
+    minNumber = userArray[0];
 
+for (int index = 0; index < userArray.Length; index++)
+{
+    if (userArray[index] > maxNumber)
+    {
+        maxNumber = userArray[index];
+    }
+    else if (userArray[index] < minNumber)
+    {
+        minNumber = userArray[index];
+    }
+}
+Console.WriteLine($"{maxNumber} - {minNumber} = {maxNumber - minNumber}");
 
+int GetSizeArray(string question)
+{
+    Console.Write(question);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+double[] GetArray(int size)
+{
+    double[] array = new double[size];
+    int wholePart = 0,
+        fractionalPart = 0;
+    for (int i = 0; i < size; i++)
+    {
+        wholePart = new Random().Next(-100, 100);
+        fractionalPart = new Random().Next(0, int.MaxValue);
+        array[i] = Convert.ToDouble(wholePart + "," + fractionalPart);
+        // Можно:
+        // array[i] = Convert.ToDouble(wholePart)/Convert.ToDouble(fractionalPart);
+        // или
+        // array[i] = wholePart + new Random().NextDouble();
+    }
+    return array;
+}
+
+void PrintArray(double[] userArray)
+{
+    string result = "[";
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (i == userArray.Length - 1)
+        {
+            result += userArray[i] + "]";
+        }
+        else
+        {
+            result += userArray[i] + ", ";
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine(result);
+    Console.WriteLine();
+}
+
+/*
 int size = Input("Введите размер массива: ");
 int[] numbers = new int[size];
 
@@ -55,3 +115,4 @@ void PrintArray(int[] array) // метод заполняющий наш мас�
      }
      Console.WriteLine();
 }
+*/
