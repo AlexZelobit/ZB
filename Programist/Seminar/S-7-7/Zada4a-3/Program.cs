4392f825ed11878534b1631958da72f88699cac9
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-07-21 10:45:49 +0300

    finished s-7-7

--- Programist/Seminar/S-7-7/Zada4a-3/Program.cs
+++ Programist/Seminar/S-7-7/Zada4a-3/Program.cs
@@ -1,33 +1,45 @@
-﻿// Задайте двумерный массив m*n заполненный случайными числами. Найти у которых оба индекса нечетные и заменить эти элементы на их квардраты
+﻿// Задайте двумерный массив m*n заполненный случайными числами. Найти эллементы у которыхоба индекса нечетные и замените эти элементы на их квадраты
+/*
+1472        1472
+5923   =>   58129
+8424        8424
+*/
+
 int m = InputInt("Введите количество строк: ");
 int n = InputInt("Введите количество столюцов: ");
 int[,] numbers = new int[m, n];
 
-FillArray(numbers);
-PrintArray(numbers);
+FirstArray(numbers);
+Console.WriteLine();
+SecondArray(numbers);
 
-void PrintArray(int[,] array)
-{
-     for(int i = 0; i < array.GetLength(0); i++) // печатаем каждый симбвол
-     {
-          for(int j = 0; j < array.GetLength(1); j++)
-          {
-               Console.Write(array[i, j] + " ");
-          }
-          Console.WriteLine(); // переходим на новую строку, чотб была таблица
-     }
-}
-
-void FillArray(int[,] array)
+void FirstArray(int[,] array) // заполняем массив
 {
      for(int i = 0; i < array.GetLength(0); i++) // получаем размер первого измерения (строк)
      {
           for(int j = 0; j < array.GetLength(1); j++) // получаем размер второго измерения (столбец)
           {
-               numbers[i, j] = new Random().Next(0,10);
+               numbers[i,j] = new Random().Next(0, 10);
+               Console.Write(array[i, j] + " ");
           }
+          Console.WriteLine();
      }
 }
+
+void SecondArray(int[,] array) // меняем элементы если индекс у них не четный 
+{
+     for(int i = 0; i < array.GetLength(0); i++) // получаем размер первого измерения (строк)
+     {
+          for(int j = 0; j < array.GetLength(1); j++) // получаем размер второго измерения (столбец)
+          {
+               if(i % 2 != 0 && j % 2 != 0)
+               numbers[i, j] *= numbers[i, j];
+               Console.Write(array[i, j] + " ");
+          }
+          Console.WriteLine();
+     }
+}
+
 
 int InputInt(string output)
 {
