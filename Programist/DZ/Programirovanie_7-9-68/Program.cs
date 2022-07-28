0811b8ebe8b419dac983de5ce42fb9056883e8b4
Александр Александрович Кармаш <ka@zb.zelobit.local>
2022-07-28 11:16:44 +0300

    DZ

--- Programist/DZ/Programirovanie_7-9-68/Program.cs
+++ Programist/DZ/Programirovanie_7-9-68/Program.cs
@@ -1,15 +1,16 @@
 ﻿// Вычислит функцию Аккермана с помощью рекурсии.
 // m=2, n=3 будет А(m,n) = 9
+// m=3, n=2 будет А(m,n) = 29
 /*
 А(m,n) = 
 n + 1 если m = 0
 А(m-1,1) если m>0, n=0
 A(m-1, A(m,n-1)) m>0, n>0
 */
-int m = 2;
-int n = 3;
+int m = 3;
+int n = 2;
 Console.Clear();
-Console.Write("При m = 2, n = 3 А(m,n) = " + recursion(m, n)); // вызов рекурсивной функции
+Console.Write($"При m = {m}, n = {n} А(m,n) = {recursion(m, n)}"); // вызов рекурсивной функции
 int recursion(int m, int n)
 {
      // Базовый случай
